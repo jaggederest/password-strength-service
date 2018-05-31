89af1cb1e11138322a6edc5971e2cb4f70cc230e
Justin George <justin.george@gmail.com>
2018-05-30 23:01:12 -0700

    Update with proper responses from spec

--- app.rb
+++ app.rb
@@ -15,11 +15,11 @@ end
 get '/password/*' do
   case Zlib::Deflate.deflate(params['splat'].to_s, 9).length
   when (0..25)
-    ['bad']
+    ['weak']
   when (25..30)
     ['ok']
   when (30..10000)
-    ['good']
+    ['strong']
   else
     [404, '']
   end
